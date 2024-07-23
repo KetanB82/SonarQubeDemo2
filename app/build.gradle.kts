d838448f1101200918e8935b61377346871c16a3
Bhangale, Ketan <kbhanga1@wm.com>
2024-07-23 19:54:02 +0530

    updated gradle 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ sonarqube {
         // new token 77391648c120b0b19469fce35639a3c518d654fc
     }
 }
-
+//Test Comments
 android {
     namespace = "com.example.sonarqubedemo2"
     compileSdk = 34
